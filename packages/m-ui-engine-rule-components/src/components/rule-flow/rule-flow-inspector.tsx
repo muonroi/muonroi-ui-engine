9393d7d5b73b9a48dde4f66bb9b63bbe1a59d145
muonroi <muonroi@outlook.com>
2026-03-21 08:06:20 +0700

    fix(quick-260321-b6b): replace hardcoded px column widths with percentage-based responsive sizing
    - MOutputContractTab Path column: 90/120px -> 25%/35%
    - MOutputContractTab Type column: 56px -> 15%
    - MOutputContractTab Actions column: 44px -> 12%
    - MFieldTable Type column: 56px -> 15%
    - tableLayout:fixed ensures remaining space distributes proportionally

--- packages/m-ui-engine-rule-components/src/components/rule-flow/rule-flow-inspector.tsx
+++ packages/m-ui-engine-rule-components/src/components/rule-flow/rule-flow-inspector.tsx
@@ -1225,10 +1225,10 @@ function MOutputContractTab({
             <table style={MTableStyle}>
               <thead>
                 <tr>
-                  <th style={{ ...MTableHeaderStyle, width: showValueExpression ? 90 : 120 }} title="Dotted path where this value is stored in the FactBag">Path</th>
-                  <th style={{ ...MTableHeaderStyle, width: 56 }} title="Data type of the output value">Type</th>
+                  <th style={{ ...MTableHeaderStyle, width: showValueExpression ? "25%" : "35%" }} title="Dotted path where this value is stored in the FactBag">Path</th>
+                  <th style={{ ...MTableHeaderStyle, width: "15%" }} title="Data type of the output value">Type</th>
                   {showValueExpression ? <th style={MTableHeaderStyle} title="FEEL/Liquid expression that computes this value at runtime">Expression</th> : null}
-                  {editable && !readOnly ? <th style={{ ...MTableHeaderStyle, width: 44 }}>Actions</th> : null}
+                  {editable && !readOnly ? <th style={{ ...MTableHeaderStyle, width: "12%" }}>Actions</th> : null}
                 </tr>
               </thead>
               <tbody>
@@ -1330,7 +1330,7 @@ function MFieldTable({
         <thead>
           <tr>
             <th style={MTableHeaderStyle}>Path</th>
-            <th style={{ ...MTableHeaderStyle, width: 56 }}>Type</th>
+            <th style={{ ...MTableHeaderStyle, width: "15%" }}>Type</th>
             <th style={MTableHeaderStyle}>Description</th>
           </tr>
         </thead>
